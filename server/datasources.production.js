a262b184e765836d24bad8672ee93a4f2cf7b793
eras0r <er4s0r@gmail.com>
2016-02-03 00:03:14 +0100

    - make datasource files environment dependent

---
+++ server/datasources.production.js
@@ -0,0 +1,17 @@
+console.log('---------- config ----------');
+console.log('host: ', process.env.OPENSHIFT_MONGODB_DB_HOST);
+console.log('port: ', process.env.OPENSHIFT_MONGODB_DB_PORT);
+console.log('user: ', process.env.OPENSHIFT_MONGODB_DB_USERNAME);
+console.log('pass: ', process.env.OPENSHIFT_MONGODB_DB_PASSWORD);
+console.log('---------- config ----------');
+
+module.exports = {
+  mongoDb: {
+    connector: 'mongodb',
+    hostname: process.env.OPENSHIFT_MONGODB_DB_HOST,
+    port: process.env.OPENSHIFT_MONGODB_DB_PORT,
+    user: process.env.OPENSHIFT_MONGODB_DB_USERNAME,
+    password: process.env.OPENSHIFT_MONGODB_DB_PASSWORD,
+    database: 'loopback-demo'
+  }
+};
