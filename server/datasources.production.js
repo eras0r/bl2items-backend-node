a118de5f54a35234a53811a8d35fe8c6872ec1fc
eras0r <er4s0r@gmail.com>
2016-02-03 23:14:04 +0100

    remove console log statements

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,10 +1,3 @@
-console.log('---------- config ----------');
-console.log('host: ', process.env.OPENSHIFT_MONGODB_DB_HOST);
-console.log('port: ', process.env.OPENSHIFT_MONGODB_DB_PORT);
-console.log('user: ', process.env.OPENSHIFT_MONGODB_DB_USERNAME);
-console.log('pass: ', process.env.OPENSHIFT_MONGODB_DB_PASSWORD);
-console.log('---------- config ----------');
-
 module.exports = {
   mongoDb: {
     connector: 'mongodb',
