bf586f93de02943ad430beb8f68071bd6db6a12d
Steven Heller <er4s0r@gmail.com>
2016-02-07 14:13:19 +0100

    improve logging in relation validator

--- server/lib/relation-validator.js
+++ server/lib/relation-validator.js
@@ -2,17 +2,10 @@ exports.validateRelation = function (propertyName, propertyValue, targetCollecti
 
   console.log('propertyName', propertyName);
   console.log('propertyValue', propertyValue);
-  //console.log('targetCollection', targetCollection);
-  //console.log('err', err);
-
-  console.log('calling exists for ' + targetCollection + ' with id= ' + propertyValue);
 
   targetCollection.exists(propertyValue, function (queryErr, exists) {
-    console.log('queryErr = ', queryErr);
-    console.log('exists = ', exists);
-
     if (!exists) {
-      console.log('Model with id %s does not exist! Throwing error now.', propertyValue);
+      console.log('%s with id "%s" does not exist! Throwing error now.', targetCollection.modelName, propertyValue);
       err();
     }
 
