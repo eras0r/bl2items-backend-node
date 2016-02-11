be5908c253361ba29e3985c684cff94c2fe9a494
eras0r <er4s0r@gmail.com>
2016-02-12 00:19:52 +0100

    introduce a generic mechanism to check for existing relations when trying to delete a document
    - the mechanism will check the defined 'hasMany' relations of the given model and prevent deletion if such relations still exist

--- server/lib/relation-validator.js
+++ server/lib/relation-validator.js
@@ -1,6 +1,8 @@
 'use strict';
 
+var Q = require('q');
 var util = require('util');
+
 var app = require('../../server/server');
 
 /**
@@ -42,3 +44,91 @@ exports.validateRelation = function (validatedCollection, propertyName, targetCo
   }
 
 };
+
+exports.ensureNoExsitingRelationsBeforeDelete = function (model) {
+
+  // before delete hook
+  model.observe('before delete', function (ctx, next) {
+
+    // check if we have an id
+    if (ctx.where && ctx.where.id) {
+      var idToDelete = ctx.where.id;
+
+      //console.log('Going to delete %s matching %j',
+      //  ctx.Model.pluralModelName,
+      //  ctx.where);
+
+      // generic test whether related objects exist (based on the models configured relations)
+
+      // iterate over relations
+      var relations = ctx.Model.relations;
+
+      // create an array of promises for the relation lookups
+      var relCountPromises = [];
+
+      for (var relKey in relations) {
+        if (relations.hasOwnProperty(relKey)) {
+          var relation = relations[relKey];
+
+          console.log('found relation with key: ', relKey);
+          var relationType = relation.type;
+          //console.log('relation type: ', relationType);
+
+          if (relationType === 'hasMany') {
+            var relationName = relation.name;
+            var modelTo = relation.modelTo;
+            //console.log('================================');
+            console.log('checking relation with name %s linking to model %s', relationName, modelTo.modelName);
+            //console.log('modelTo: ', modelTo.modelName);
+
+            relCountPromises.push(checkForExistingModelRelation(modelTo, idToDelete));
+
+          }
+        }
+      }
+      console.log('================================');
+
+      Q.all(relCountPromises)
+        .then(function (results) {
+          console.log('No relations found. Manufacturer can be deleted');
+          // just delete the manufacturer
+          next();
+        })
+        .catch(function (error) {
+          var errorMsg = util.format('Document cannot be deleted, because there are still other documents referencing to %s with id %s', model.modelName, idToDelete);
+          // throw an error
+          var err = new Error(errorMsg);
+          err.statusCode = 422;
+          console.log(err.toString());
+          next(err);
+        });
+
+    }
+
+  });
+};
+
+function checkForExistingModelRelation(targetModel, idToSearch) {
+
+  console.log('check for existing model relation with model %s and id %s', targetModel.modelName, idToSearch);
+  var deferred = Q.defer();
+  // TODO count should be executed in parallel if there are multiple relations
+  console.log('checking for relation of model %s with id %s', targetModel.modelName, idToSearch);
+  targetModel.count({manufacturerId: idToSearch})
+    .then(function (relatedItemsCount) {
+      console.log('number of related items for model %s: %d', targetModel.modelName, relatedItemsCount);
+      if (relatedItemsCount === 0) {
+        deferred.resolve(relatedItemsCount);
+      }
+      else {
+        deferred.reject(util.format('There are still %d number of %s', relatedItemsCount, targetModel.modelName));
+      }
+    })
+    .catch(function (error) {
+      // TODO allow deletion or not if query could not be executed?
+      console.log('error searching models: ', error);
+      deferred.reject('Errors checking for existing relation for target model %s with id: ', targetModel.modelName, idToSearch);
+    });
+
+  return deferred.promise;
+}
