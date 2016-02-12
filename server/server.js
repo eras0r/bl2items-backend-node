6cd4520ce6fe9e87300f9513752dd0dbfe124f6b
Steven Heller <er4s0r@gmail.com>
2016-02-12 12:11:37 +0100

    proper use of logger

--- server/server.js
+++ server/server.js
@@ -1,27 +1,35 @@
+'use strict';
+
 var loopback = require('loopback');
 var boot = require('loopback-boot');
 
+var log = require('./middleware/logger');
+
 var app = module.exports = loopback();
 
-app.start = function() {
+app.start = function () {
   // start the web server
-  return app.listen(function() {
+  return app.listen(function () {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
+
+    log.info('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+      log.info('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
   });
 };
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
+boot(app, __dirname, function (err) {
+  if (err) {
+    throw err;
+  }
 
   // start the server if `$ node server.js`
-  if (require.main === module)
+  if (require.main === module) {
     app.start();
+  }
 });
