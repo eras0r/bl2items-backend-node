0e4cb177401b4c8bfe2acd89ddfdf25ebd849e8a
Steven Heller <er4s0r@gmail.com>
2016-10-22 07:35:51 +0200

    add create-users boot script

---
+++ server/boot/create-users.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var log = require('./../middleware/logger');
+
+module.exports = function (app) {
+
+ if (process.env.NODE_ENV === 'development') {
+
+   var User = app.models.Bl2User;
+   var Role = app.models.Bl2Role;
+   var RoleMapping = app.models.Bl2RoleMapping;
+
+   app.dataSources.mongoDb.automigrate('Bl2User', function (err) {
+     if (err) {
+       throw err;
+     }
+
+     User.create([
+       {
+         username: 'admin',
+         email: 'admin@example.com',
+         password: 'password' // TODO
+       },
+       {
+         username: 'user',
+         email: 'user@example.com',
+         password: 'password' // TODO
+       }
+     ], function (err, users) {
+       if (err) {
+         throw err;
+       }
+
+       // delete all roles
+       Role.destroyAll(function (err, info) {
+
+         // delete all role mappings
+         RoleMapping.destroyAll(function (err, info) {
+
+           //create the admin role
+           Role.create({
+             name: 'admin'
+           }, function (err, role) {
+             if (err) {
+               throw err;
+             }
+
+             log.debug('Created role:', role);
+
+             //make 'admin' an admin
+             role.principals.create({
+               principalType: RoleMapping.USER,
+               principalId: users[0].id
+             }, function (err, principal) {
+               if (err) throw err;
+
+               log.debug('Created principal:', principal);
+             });
+           });
+
+         });
+
+       });
+
+       log.debug('Number of Users created: %d', Object.keys(users).length);
+     });
+
+   });
+
+ }
+
+};
